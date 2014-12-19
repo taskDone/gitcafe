9d76db060bfe40a2f1f76fde73a627837b642857
taskDone <kdrj04@139.com>
2014-12-19 18:27:14 +0800

    new or clone ui

--- src/GitCafeModule.Working/ViewModels/Working.cs
+++ src/GitCafeModule.Working/ViewModels/Working.cs
@@ -1,10 +1,12 @@
 ﻿using GitCafeCommon.PresentationEvent;
+using Microsoft.Practices.Prism.Commands;
 using Microsoft.Practices.Prism.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows;
+using System.Windows.Input;
 
 namespace GitCafeModule.Working.ViewModels
 {
@@ -19,12 +21,14 @@ namespace GitCafeModule.Working.ViewModels
 
             NewOrClonePopupVisibility = Visibility.Hidden;
 
+            NewOrCloneCacelCommand = new DelegateCommand(HideNewOrCloneDialog);
+
             var barClickEvent = eventAggregator.GetEvent<ToolBarClickEvent>();
             if (recevieToolBarClickToken != null)
             {
                 barClickEvent.Unsubscribe(recevieToolBarClickToken);
             }
-            recevieToolBarClickToken = barClickEvent.Subscribe(ShowNewOrCloneDialog, ThreadOption.UIThread, false);
+            recevieToolBarClickToken = barClickEvent.Subscribe(ToolBarHandler, ThreadOption.UIThread, false);
         }
 
         public Visibility NewOrClonePopupVisibility
@@ -32,12 +36,32 @@ namespace GitCafeModule.Working.ViewModels
             get { return GetValue(() => NewOrClonePopupVisibility); }
             set { SetValue(() => NewOrClonePopupVisibility, value); }
         }
-        private void ShowNewOrCloneDialog(ToolBarClickType clickType)
+
+        public ICommand NewOrCloneCacelCommand { get;private set; }
+
+        private void ToolBarHandler(ToolBarClickType clickType)
         {
             if (clickType == ToolBarClickType.NewOrClone)
             {
-                NewOrClonePopupVisibility = Visibility.Visible;
+                ShowNewOrCloneDialog(true);
             }            
         }
+        #region NewOrCloneDialog
+        private void HideNewOrCloneDialog()
+        {
+            ShowNewOrCloneDialog(false);
+        }
+        private void ShowNewOrCloneDialog(bool isShow)
+        {
+            if (isShow)
+            {
+                NewOrClonePopupVisibility = Visibility.Visible;
+            }
+            else
+            {
+                NewOrClonePopupVisibility = Visibility.Collapsed;
+            }
+        }
+        #endregion
     }
 }
